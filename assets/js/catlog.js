f3abd3fc030d0524743f8a2046e1c3bf74471018
hafizkachara <161581500+hafizkachara@users.noreply.github.com>
2025-07-31 15:35:55 +0530

    Add

---
+++ assets/js/catlog.js
@@ -0,0 +1,334 @@
+function Myfunc_cat() {
+    let category_btn = document.getElementById("btn-toggle_cate");
+    let season_btn = document.getElementById("btn-toggle_blous");
+    let size_btn = document.getElementById("btn-toggle_size");
+    let color_btn = document.getElementById("btn-toggle_color");
+    let brand_btn = document.getElementById("btn-toggle_brand");
+    let price_btn = document.getElementById("btn-toggle_price");
+
+    if (category_btn.style.display == "flex") {
+        category_btn.style.display = "none"
+    } else {
+        category_btn.style.display = "flex";
+        season_btn.style.display = "nonne";
+        size_btn.style.display = "none";
+        color_btn.style.display = "nonne";
+        brand_btn.style.display = "none";
+        price_btn.style.display = "none";
+    }
+}
+function Myfunc_season() {
+    let category_btn = document.getElementById("btn-toggle_cate");
+    let season_btn = document.getElementById("btn-toggle_blous");
+    let size_btn = document.getElementById("btn-toggle_size");
+    let color_btn = document.getElementById("btn-toggle_color");
+    let brand_btn = document.getElementById("btn-toggle_brand");
+    let price_btn = document.getElementById("btn-toggle_price");
+
+    if (season_btn.style.display == "flex") {
+        season_btn.style.display = "none"
+    } else {
+        season_btn.style.display = "flex";
+        category_btn.style.display = "nonne";
+        size_btn.style.display = "none";
+        color_btn.style.display = "nonne";
+        brand_btn.style.display = "none";
+        price_btn.style.display = "none";
+    }
+}
+function Myfunc_size() {
+    let category_btn = document.getElementById("btn-toggle_cate");
+    let season_btn = document.getElementById("btn-toggle_blous");
+    let size_btn = document.getElementById("btn-toggle_size");
+    let color_btn = document.getElementById("btn-toggle_color");
+    let brand_btn = document.getElementById("btn-toggle_brand");
+    let price_btn = document.getElementById("btn-toggle_price");
+
+    if (size_btn.style.display == "flex") {
+        size_btn.style.display = "none"
+    } else {
+        size_btn.style.display = "flex";
+        category_btn.style.display = "nonne";
+        season_btn.style.display = "nonne";
+        color_btn.style.display = "nonne";
+        brand_btn.style.display = "none";
+        price_btn.style.display = "none";
+    }
+}
+function Myfunc_color() {
+    let category_btn = document.getElementById("btn-toggle_cate");
+    let season_btn = document.getElementById("btn-toggle_blous");
+    let size_btn = document.getElementById("btn-toggle_size");
+    let color_btn = document.getElementById("btn-toggle_color");
+    let brand_btn = document.getElementById("btn-toggle_brand");
+    let price_btn = document.getElementById("btn-toggle_price");
+
+    if (color_btn.style.display == "flex") {
+        color_btn.style.display = "none"
+    } else {
+        color_btn.style.display = "flex";
+        category_btn.style.display = "nonne";
+        season_btn.style.display = "nonne";
+        size_btn.style.display = "nonne";
+        brand_btn.style.display = "none";
+        price_btn.style.display = "none";
+    }
+}
+function Myfunc_brand() {
+    let category_btn = document.getElementById("btn-toggle_cate");
+    let season_btn = document.getElementById("btn-toggle_blous");
+    let size_btn = document.getElementById("btn-toggle_size");
+    let color_btn = document.getElementById("btn-toggle_color");
+    let brand_btn = document.getElementById("btn-toggle_brand");
+    let price_btn = document.getElementById("btn-toggle_price");
+
+    if (brand_btn.style.display == "flex") {
+        brand_btn.style.display = "none"
+    } else {
+        brand_btn.style.display = "flex";
+        category_btn.style.display = "nonne";
+        season_btn.style.display = "nonne";
+        size_btn.style.display = "nonne";
+        color_btn.style.display = "nonne";
+        price_btn.style.display = "none";
+    }
+}
+function Myfunc_price() {
+    let category_btn = document.getElementById("btn-toggle_cate");
+    let season_btn = document.getElementById("btn-toggle_blous");
+    let size_btn = document.getElementById("btn-toggle_size");
+    let color_btn = document.getElementById("btn-toggle_color");
+    let brand_btn = document.getElementById("btn-toggle_brand");
+    let price_btn = document.getElementById("btn-toggle_price");
+
+    if (price_btn.style.display == "flex") {
+        price_btn.style.display = "none"
+    } else {
+        price_btn.style.display = "flex";
+        category_btn.style.display = "nonne";
+        season_btn.style.display = "nonne";
+        size_btn.style.display = "nonne";
+        color_btn.style.display = "nonne";
+        brand_btn.style.display = "none";
+    }
+}
+// ment inner cat
+function Myfunc_blous() {
+    let btn_toggle_blous_2 = document.getElementById("btn-toggle_blous_2");
+    let btn_toggle_coats_2 = document.getElementById("btn-toggle_coats_2");
+    let btn_toggle_dress_2 = document.getElementById("btn-toggle_dress_2");
+    let btn_toggle_hoodies_2 = document.getElementById("btn-toggle_hoodies_2");
+    let btn_toggle_denim_2 = document.getElementById("btn-toggle_denim_2");
+    let btn_toggle_jeans_2 = document.getElementById("btn-toggle_jeans_2");
+    let btn_toggle_jumper_2 = document.getElementById("btn-toggle_jumper_2");
+    let btn_toggle_leggings_2 = document.getElementById("btn-toggle_leggings_2");
+
+    if (btn_toggle_blous_2.style.display == "flex") {
+        btn_toggle_blous_2.style.display = "none";
+    } else {
+        btn_toggle_blous_2.style.display = "flex";
+        btn_toggle_coats_2.style.display = "none";
+        btn_toggle_dress_2.style.display = "none";
+        btn_toggle_hoodies_2.style.display = "none";
+        btn_toggle_denim_2.style.display = "none";
+        btn_toggle_jeans_2.style.display = "none";
+        btn_toggle_jumper_2.style.display = "none";
+        btn_toggle_leggings_2.style.display = "none";
+    }
+}
+function Myfunc_coats() {
+    let btn_toggle_blous_2 = document.getElementById("btn-toggle_blous_2");
+    let btn_toggle_coats_2 = document.getElementById("btn-toggle_coats_2");
+    let btn_toggle_dress_2 = document.getElementById("btn-toggle_dress_2");
+    let btn_toggle_hoodies_2 = document.getElementById("btn-toggle_hoodies_2");
+    let btn_toggle_denim_2 = document.getElementById("btn-toggle_denim_2");
+    let btn_toggle_jeans_2 = document.getElementById("btn-toggle_jeans_2");
+    let btn_toggle_jumper_2 = document.getElementById("btn-toggle_jumper_2");
+    let btn_toggle_leggings_2 = document.getElementById("btn-toggle_leggings_2");
+    if (btn_toggle_coats_2.style.display == "flex") {
+        btn_toggle_coats_2.style.display = "none"
+    } else {
+        btn_toggle_coats_2.style.display = "flex";
+        btn_toggle_blous_2.style.display = "none";
+        btn_toggle_dress_2.style.display = "none";
+        btn_toggle_hoodies_2.style.display = "none";
+        btn_toggle_denim_2.style.display = "none";
+        btn_toggle_jeans_2.style.display = "none";
+        btn_toggle_jumper_2.style.display = "none";
+        btn_toggle_leggings_2.style.display = "none";
+    }
+}
+function Myfunc_dresses() {
+    let btn_toggle_blous_2 = document.getElementById("btn-toggle_blous_2");
+    let btn_toggle_coats_2 = document.getElementById("btn-toggle_coats_2");
+    let btn_toggle_dress_2 = document.getElementById("btn-toggle_dress_2");
+    let btn_toggle_hoodies_2 = document.getElementById("btn-toggle_hoodies_2");
+    let btn_toggle_denim_2 = document.getElementById("btn-toggle_denim_2");
+    let btn_toggle_jeans_2 = document.getElementById("btn-toggle_jeans_2");
+    let btn_toggle_jumper_2 = document.getElementById("btn-toggle_jumper_2");
+    let btn_toggle_leggings_2 = document.getElementById("btn-toggle_leggings_2");
+    if (btn_toggle_dress_2.style.display == "flex") {
+        btn_toggle_dress_2.style.display = "none"
+
+    } else {
+        btn_toggle_dress_2.style.display = "flex";
+        btn_toggle_blous_2.style.display = "none";
+        btn_toggle_coats_2.style.display = "none";
+        btn_toggle_hoodies_2.style.display = "none";
+        btn_toggle_denim_2.style.display = "none";
+        btn_toggle_jeans_2.style.display = "none";
+        btn_toggle_jumper_2.style.display = "none";
+        btn_toggle_leggings_2.style.display = "none";
+    }
+}
+function Myfunc_hoodies() {
+    let btn_toggle_blous_2 = document.getElementById("btn-toggle_blous_2");
+    let btn_toggle_coats_2 = document.getElementById("btn-toggle_coats_2");
+    let btn_toggle_dress_2 = document.getElementById("btn-toggle_dress_2");
+    let btn_toggle_hoodies_2 = document.getElementById("btn-toggle_hoodies_2");
+    let btn_toggle_denim_2 = document.getElementById("btn-toggle_denim_2");
+    let btn_toggle_jeans_2 = document.getElementById("btn-toggle_jeans_2");
+    let btn_toggle_jumper_2 = document.getElementById("btn-toggle_jumper_2");
+    let btn_toggle_leggings_2 = document.getElementById("btn-toggle_leggings_2");
+    if (btn_toggle_hoodies_2.style.display == "flex") {
+        btn_toggle_hoodies_2.style.display = "none"
+
+    } else {
+        btn_toggle_hoodies_2.style.display = "flex";
+        btn_toggle_blous_2.style.display = "none";
+        btn_toggle_coats_2.style.display = "none";
+        btn_toggle_dress_2.style.display = "none";
+        btn_toggle_denim_2.style.display = "none";
+        btn_toggle_jeans_2.style.display = "none";
+        btn_toggle_jumper_2.style.display = "none";
+        btn_toggle_leggings_2.style.display = "none";
+    }
+}
+
+function Myfunc_denim() {
+    let btn_toggle_blous_2 = document.getElementById("btn-toggle_blous_2");
+    let btn_toggle_coats_2 = document.getElementById("btn-toggle_coats_2");
+    let btn_toggle_dress_2 = document.getElementById("btn-toggle_dress_2");
+    let btn_toggle_hoodies_2 = document.getElementById("btn-toggle_hoodies_2");
+    let btn_toggle_denim_2 = document.getElementById("btn-toggle_denim_2");
+    let btn_toggle_jeans_2 = document.getElementById("btn-toggle_jeans_2");
+    let btn_toggle_jumper_2 = document.getElementById("btn-toggle_jumper_2");
+    let btn_toggle_leggings_2 = document.getElementById("btn-toggle_leggings_2");
+    if (btn_toggle_denim_2.style.display == "flex") {
+        btn_toggle_denim_2.style.display = "none"
+
+    } else {
+        btn_toggle_denim_2.style.display = "flex";
+        btn_toggle_blous_2.style.display = "none";
+        btn_toggle_coats_2.style.display = "none";
+        btn_toggle_dress_2.style.display = "none";
+        btn_toggle_hoodies_2.style.display = "none";
+        btn_toggle_jeans_2.style.display = "none";
+        btn_toggle_jumper_2.style.display = "none";
+        btn_toggle_leggings_2.style.display = "none";
+
+    }
+}
+
+function Myfunc_jeans() {
+    let btn_toggle_blous_2 = document.getElementById("btn-toggle_blous_2");
+    let btn_toggle_coats_2 = document.getElementById("btn-toggle_coats_2");
+    let btn_toggle_dress_2 = document.getElementById("btn-toggle_dress_2");
+    let btn_toggle_hoodies_2 = document.getElementById("btn-toggle_hoodies_2");
+    let btn_toggle_denim_2 = document.getElementById("btn-toggle_denim_2");
+    let btn_toggle_jeans_2 = document.getElementById("btn-toggle_jeans_2");
+    let btn_toggle_jumper_2 = document.getElementById("btn-toggle_jumper_2");
+    let btn_toggle_leggings_2 = document.getElementById("btn-toggle_leggings_2")
+    if (btn_toggle_jeans_2.style.display == "flex") {
+        btn_toggle_jeans_2.style.display = "none"
+
+    } else {
+        btn_toggle_jeans_2.style.display = "flex";
+        btn_toggle_blous_2.style.display = "none";
+        btn_toggle_coats_2.style.display = "none";
+        btn_toggle_dress_2.style.display = "none";
+        btn_toggle_hoodies_2.style.display = "none";
+        btn_toggle_denim_2.style.display = "none";
+        btn_toggle_jumper_2.style.display = "none";
+        btn_toggle_leggings_2.style.display = "none";
+    }
+}
+
+function Myfunc_jumper() {
+    let btn_toggle_blous_2 = document.getElementById("btn-toggle_blous_2");
+    let btn_toggle_coats_2 = document.getElementById("btn-toggle_coats_2");
+    let btn_toggle_dress_2 = document.getElementById("btn-toggle_dress_2");
+    let btn_toggle_hoodies_2 = document.getElementById("btn-toggle_hoodies_2");
+    let btn_toggle_denim_2 = document.getElementById("btn-toggle_denim_2");
+    let btn_toggle_jeans_2 = document.getElementById("btn-toggle_jeans_2");
+    let btn_toggle_jumper_2 = document.getElementById("btn-toggle_jumper_2");
+    let btn_toggle_leggings_2 = document.getElementById("btn-toggle_leggings_2")
+    if (btn_toggle_jumper_2.style.display == "flex") {
+        btn_toggle_jumper_2.style.display = "none"
+    } else {
+        btn_toggle_jumper_2.style.display = "flex";
+        btn_toggle_blous_2.style.display = "none";
+        btn_toggle_coats_2.style.display = "none";
+        btn_toggle_dress_2.style.display = "none";
+        btn_toggle_hoodies_2.style.display = "none";
+        btn_toggle_denim_2.style.display = "none";
+        btn_toggle_jeans_2.style.display = "none";
+        btn_toggle_leggings_2.style.display = "none";
+    }
+}
+
+function Myfunc_leggings() {
+    let btn_toggle_blous_2 = document.getElementById("btn-toggle_blous_2");
+    let btn_toggle_coats_2 = document.getElementById("btn-toggle_coats_2");
+    let btn_toggle_dress_2 = document.getElementById("btn-toggle_dress_2");
+    let btn_toggle_hoodies_2 = document.getElementById("btn-toggle_hoodies_2");
+    let btn_toggle_denim_2 = document.getElementById("btn-toggle_denim_2");
+    let btn_toggle_jeans_2 = document.getElementById("btn-toggle_jeans_2");
+    let btn_toggle_jumper_2 = document.getElementById("btn-toggle_jumper_2");
+    let btn_toggle_leggings_2 = document.getElementById("btn-toggle_leggings_2")
+    if (btn_toggle_leggings_2.style.display == "flex") {
+        btn_toggle_leggings_2.style.display = "none"
+    } else {
+        btn_toggle_leggings_2.style.display = "flex";
+        btn_toggle_blous_2.style.display = "none";
+        btn_toggle_coats_2.style.display = "none";
+        btn_toggle_dress_2.style.display = "none";
+        btn_toggle_hoodies_2.style.display = "none";
+        btn_toggle_denim_2.style.display = "none";
+        btn_toggle_jeans_2.style.display = "none";
+        btn_toggle_jumper_2.style.display = "none";
+
+    }
+}
+
+
+
+// slider 
+// strat
+    function Myfunc_card_1() {
+        let card_1 = document.getElementById("card_1");
+        let card_2 = document.getElementById("card_2");
+        let card_3 = document.getElementById("card_3");
+        
+        if (card_1.style.display = "block") {
+           card_1.style.display = "none";
+        } else {
+            card_1.style.display = "block";
+            card_2.style.display = "none";
+            card_3.style.display = "none";
+        }
+    }
+    function Myfunc_card_2 () {
+        let card_1 = document.getElementById("card_1");
+        let card_2 = document.getElementById("card_2");
+        let card_3 = document.getElementById("card_3");
+
+        if (card_2.style.display = "block") {
+           card_2.style.display = "none";
+        } else {
+            card_2.style.display = "block";
+            card_1.style.display = "none";
+            card_3.style.display = "none";
+        }
+    }
+// end
